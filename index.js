07088c787d9ada0a914284a246315d88cd471986
Caystone99 <emmanuelcaleb546@gmail.com>
2022-09-14 23:29:08 +0100

    index

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./src/app');
+const config = require('./api/utils/config');
+const http = require('http');
+
+
+let server = http.createServer(app);
+
+server.listen(config.PORT, ()=>{
+    console.log(`Server is running on PORT ${config.PORT}`)
+})
