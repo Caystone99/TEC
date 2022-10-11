2fb74e021c38cfa2cbce244592ce6519993da5ad
Caystone99 <emmanuelcaleb546@gmail.com>
2022-10-11 23:59:07 +0100

    application
    Application form initial design

--- public/js/apply.js
+++ public/js/apply.js
@@ -1,10 +1,54 @@
-/*
-const applyBody = document.getElementById('applyBody');
+let personal = document.getElementById('personal');
+let personFw = document.getElementById('personFw');
 
-const admitBody = `
+let academic = document.getElementById('academic');
+let personBk = document.getElementById('personBk');
+let academicFw = document.getElementById('academicFw');
 
-`
+let study = document.getElementById('study');
+let studyBk = document.getElementById('studyBk');
 
-applyBody.insertAdjacentHTML('afterend', admitBody)
+let oLevel = document.getElementById('oLevel');
+let addBtn = document.getElementById('addInput');
+let inputCount = 1;
 
-*/
+personFw.addEventListener('click', ()=>{
+    personal.style.WebkitAnimation = "slideOut 4s 2"
+    personal.style.animation = "slideOut 4s 2" 
+    personal.style.display = "none"
+    academic.style.display = "flex"
+})
+
+personBk.addEventListener('click', ()=>{
+    personal.style.display = "flex"
+    academic.style.display = "none"
+})
+
+academicFw.addEventListener('click', ()=>{
+    academic.style.display = "none"
+    study.style.display = "flex"
+})
+
+studyBk.addEventListener('click', ()=>{
+    study.style.display = "none"
+    academic.style.display = "flex"
+})
+
+const formInput = count =>{
+    return `
+    <div class="flex flex-col space-y-2 items-start ">
+        <label for="o_level${count}" class="block text-sm font-medium"> O'Level Certificate </label>
+        <input 
+            type="file"
+            name="o_level${count}"
+            class="w-[80vw] md:w-[400px] px-4 py-2 text-sm border border-armyGreen rounded-md focus:outline-none focus:ring-1 focus:ring-armyGreen"
+        />
+    </div>
+    `
+}
+
+addBtn.addEventListener('click', ()=>{
+    inputCount += 1;
+    oLevel.insertAdjacentHTML('afterend', formInput(inputCount))
+    addBtn.style.display = "none"
+})
